e4f9f5ea5b13e7aac00120ed5bd4604b3a916fe9
Vovan_Super <ovsyukov@yandex.com>
2018-05-14 07:31:01 +0000

    changes index to kandorapp index

--- index.js
+++ index.js
@@ -1,182 +1,195 @@
-const util = require('util')
-const fs = require('fs')
+const util = require('util');
+const fs = require('fs');
 
-const dotenv = require('dotenv')
-const express = require('express')
-const ipfsApi = require('ipfs-api')
-const multer = require('multer')
-const helmet = require('helmet')
+const dotenv = require('dotenv');
+const express = require('express');
+const ipfsApi = require('ipfs-api');
+const multer = require('multer');
+const helmet = require('helmet');
 
-dotenv.load()
+dotenv.load();
 
-const ipfs = ipfsApi(process.env.IPFS_NODE_HOST, process.env.IPFS_NODE_PORT, { protocol: 'http' })
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+let codeToHashMap = new Map();
+let codeToDigestMap = new Map();
+var ipfsLocation = "";
 
-const hashCounterFilename = __dirname + '/hashCounter.json'
+const ipfs = ipfsApi(process.env.IPFS_NODE_HOST, process.env.IPFS_NODE_PORT, { protocol: 'http' });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-const app = express()
-app.use(helmet())
+const hashCounterFilename = __dirname + '/hashCounter.json';
 
-const uploadFile = upload.single('file')
-const byKeyRouter = require('./routes/bykey.route.js');
-
-
-
-
-
-
-app.use('/bykey', byKeyRouter);
+const app = express();
+app.use(helmet());
 
 app.get('/ipfs/:hash', (req, res, next) => {
+  console.log(codeToHashMap)
+  var ipfsLocation = "";
+  var digest = "";
+  let hash = req.params.hash;
+  console.log("hash", hash);
+  if (hash.charAt(0) != "Q") {
+    ipfsLocation = codeToHashMap.get(hash);
+    digest = codeToDigestMap.get(hash);
+  }
+  else {
+    ipfsLocation = hash;
+  };
 
-  checkRateLimit(req.params.hash)
+
+
+  console.log("hash", hash);
+  if (hash.charAt(0) != "Q") {
+    ipfsLocation = codeToHashMap.get(hash);
+    digest = codeToDigestMap.get(hash);
+  }
+  else {
+    ipfsLocation = hash;
+  }
+
+
+  console.log("cTHP.get(hash)", codeToHashMap.get(hash));
+  console.log("ipfsLocation", ipfsLocation);
+  checkRateLimit(ipfsLocation)
     .then(isRateLimitReached => {
       if (!isRateLimitReached) {
-        return res.status(403).json({ error: 'Rate limit reached. The resource is not anymore available' })
+        return res.status(403).json({ error: 'Rate limit reached. The resource is not anymore available' });
       }
-      ipfs.files.cat(req.params.hash, (err, data) => {
+      ipfs.files.cat(ipfsLocation, (err, data) => {
         if (err) {
-          return res.status(500).json({ error: util.format(err) })
+          return res.status(500).json({ error: util.format(err) });
         }
 
-        decrementAllowedDownloads(req.params.hash)
+        decrementAllowedDownloads(ipfsLocation)
           .then(() => {
-            res.setHeader('Content-Type', 'application/octet-stream')
-            res.setHeader('Content-Disposition', 'attachment; filename=' + req.params.hash)
-            res.send(data)
+            res.setHeader('Content-Type', 'application/octet-stream');
+            res.setHeader('Content-Disposition', 'attachment; filename=' + ipfsLocation);
+            res.send(data);
           })
-          .catch(err => res.status(500).json({ error: util.format(err) }))
+          .catch(err => res.status(500).json({ error: util.format(err) }));
 
-      })
+      });
     })
-    .catch(err => res.status(500).json({ error: util.format(err) }))
-})
+    .catch(err => res.status(500).json({ error: util.format(err) }));
+});
 
 
+const uploadFile = upload.single('file');
 
 app.post('/ipfs', (req, res) => {
   uploadFile(req, res, err => {
     if (err) {
-      return res.status(400).json({ error: util.format(err) })
+      return res.status(400).json({ error: util.format(err) });
     }
     if (!req.file) {
-      return res.status(400).json({ error: 'File form data parameter is missing' })
+      return res.status(400).json({ error: 'File form data parameter is missing' });
     }
 
     if (!req.body.rateLimit) {
-      return res.status(400).json({ error: 'rateLimit parameter is missing' })
+      return res.status(400).json({ error: 'rateLimit parameter is missing' });
     }
 
     ipfs.files.add(req.file.buffer)
       .then(result => {
-        const ipfsResponse = result[0]
+        const ipfsResponse = result[0];
+        codeToHashMap.set(req.body.keyCode, ipfsResponse.hash);
+        codeToDigestMap.set(req.body.keyCode, req.body.digest);
+        console.log(codeToHashMap);
         storeHashCounter(ipfsResponse.hash, req.body.rateLimit)
           .then(() => res.json({
             hash: ipfsResponse.hash,
             size: ipfsResponse.size,
-            rateLimit: req.body.rateLimit
+            digest: req.body.digest,
+            rateLimit: req.body.rateLimit,
+            keyCode: req.body.keyCode
           }))
-          .catch(err => res.status(500).json({ error: util.format(err) }))
+          .catch(err => res.status(500).json({ error: util.format(err) }));
       })
-      .catch(err => res.status(500).json({ error: util.format(err) }))
-  })
+      .catch(err => res.status(500).json({ error: util.format(err) }));
+  });
 });
-
-app.get('/', (res, resp) => {
-  resp.status(200).json({
-    status: 'Ok',
-    routes: [
-      'GET /bykey/ipfs/:keycode',
-      'POST /bykey/ipfs/:keycode',
-      'GET /ipfs/:hash',
-      'POST /ipfs/:hash',
-      ]
-  })
-})
-
-
 
 function checkRateLimit(hash) {
   return new Promise((resolve, reject) => {
     fs.exists(hashCounterFilename, exists => {
       if (!exists) {
-        return reject('Not rate limit was given for this file')
+        return reject('Not rate limit was given for this file');
       }
       fs.readFile(hashCounterFilename, { encoding: 'utf8' }, (err, data) => {
         if (err) {
-          return reject(err)
+          return reject(err);
         }
-        const hashCounterData = JSON.parse(data)
-        if (hashCounterData[hash] === undefined) return resolve(true)
-        else if (hashCounterData[hash] === 0) return resolve(false)
-        else resolve(true)
-      })
-    })
-  })
+        const hashCounterData = JSON.parse(data);
+        if (hashCounterData[hash] === undefined) return resolve(true);
+        else if (hashCounterData[hash] === 0) return resolve(false);
+        else resolve(true);
+      });
+    });
+  });
 }
+
 
 function decrementAllowedDownloads(hash) {
   return new Promise((resolve, reject) => {
     fs.readFile(hashCounterFilename, { encoding: 'utf8' }, (err, data) => {
       if (err) {
-        return reject(err)
+        return reject(err);
       }
-      const hashCounterData = JSON.parse(data)
+      const hashCounterData = JSON.parse(data);
       if (hashCounterData[hash] > 0) {
-        hashCounterData[hash] -= 1
+        hashCounterData[hash] -= 1;
         writeCounterOnFile(hashCounterData)
           .then(resolve)
-          .catch(reject)
+          .catch(reject);
       }
       else {
-        resolve()
+        resolve();
       }
-    })
-  })
+    });
+  });
 }
 
 function storeHashCounter(hash, counter) {
   return new Promise((resolve, reject) => {
     fs.exists(hashCounterFilename, exists => {
-      let hashCounterData = {}
+      let hashCounterData = {};
       if (exists) {
         fs.readFile(hashCounterFilename, { encoding: 'utf8' }, (err, data) => {
           if (err) {
-            return reject(err)
+            return reject(err);
           }
-          hashCounterData = JSON.parse(data)
-          hashCounterData[hash] = counter
+          hashCounterData = JSON.parse(data);
+          hashCounterData[hash] = counter;
           return writeCounterOnFile(hashCounterData)
             .then(resolve)
-            .catch(reject)
-        })
+            .catch(reject);
+        });
       }
       else {
-        hashCounterData[hash] = counter
+        hashCounterData[hash] = counter;
         writeCounterOnFile(hashCounterData)
           .then(resolve)
-          .catch(reject)
+          .catch(reject);
       }
 
-    })
-  })
+    });
+  });
 }
 
 function writeCounterOnFile(hashCounterData) {
   return new Promise((resolve, reject) => {
     fs.writeFile(hashCounterFilename, JSON.stringify(hashCounterData), { encoding: 'utf8' }, err => {
       if (err) {
-        return reject(err)
+        return reject(err);
       }
-      resolve()
-    })
-  })
+      resolve();
+    });
+  });
 }
 
 app.listen(process.env.PORT, () => {
-  console.log('API listening on ' + process.env.PORT)
-})
+  console.log('API listening on ' + process.env.PORT);
+});
 
 
-module.exports = app
+module.exports = app;
